d47cac29b4be7dc9839512f17214b46db000cefb
Cedric <wangcedric272@gmail.com>
2024-01-30 10:23:28 -0500

    Add user route to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-
 import Home from "pages/Home";
 import MyInterview from "pages/MyInterview";
 import Profile from "pages/Profile";
 import Login from "pages/Login";
+import User from "pages/User"
 
 const AppRoutes = () => {
   return (
@@ -12,6 +12,7 @@ const AppRoutes = () => {
       <Route path="/my-interviews" element={<MyInterview />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/users" element={<User />} />
     </Routes>
   );
 };
