263ec21851aef15966e301075ad0382b72f4cf84
Cedric <wangcedric272@gmail.com>
2024-02-06 04:17:56 -0500

    update display label on video component on my-interviews page

--- src/pages/MyInterview/index.tsx
+++ src/pages/MyInterview/index.tsx
@@ -35,7 +35,7 @@ const MyInterview: React.FC = () => {
                 navigator(`/interviews/${interview.id}/detail`);
               }}>
                 <Typography style={{ color: "white" }}>
-                  {interview.name} - {interview.user_id}
+                {interview.user.name} : {interview.name}
                 </Typography>
                 <Box
                   component="img"
