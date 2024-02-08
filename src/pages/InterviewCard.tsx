15922d545c6e55584ee3fa9e596508fe3750316e
unknown <mickl0102@outlook.com>
2024-02-08 03:45:56 -0600

    Updated UI

--- src/pages/InterviewCard.tsx
+++ src/pages/InterviewCard.tsx
@@ -4,6 +4,7 @@ import SupportAgentIcon from "@mui/icons-material/SupportAgent";
 import { useNavigate } from "react-router-dom";
 import * as api from "api";
 import { InterviewDetailType } from "api/types";
+import { convertSecondsToHMS } from "utils/convertSecondToHMS";
 import "pages/InterviewCard.css";
 
 const InterviewCard: React.FC<{ interview: InterviewDetailType }> = ({
@@ -12,7 +13,7 @@ const InterviewCard: React.FC<{ interview: InterviewDetailType }> = ({
   const navigator = useNavigate();
 
   return (
-    <Grid item xs={2} key={interview.id}>
+    <Grid item xs={2} key={interview.id} sx={{ marginBottom: "20px" }}>
       <Box
         onClick={() => {
           navigator(`/interviews/${interview.id}/detail`);
@@ -38,7 +39,7 @@ const InterviewCard: React.FC<{ interview: InterviewDetailType }> = ({
             display: "flex",
           }}
         >
-          <SupportAgentIcon sx={{ marginRight: 1 }} />
+          <SupportAgentIcon sx={{ marginRight: "5px" }} />
           {interview.user.name}
         </Box>
         <Box
@@ -47,8 +48,8 @@ const InterviewCard: React.FC<{ interview: InterviewDetailType }> = ({
             display: "flex",
           }}
         >
-          <BusinessIcon sx={{ marginRight: 1 }} />
-          {interview.name}
+          <BusinessIcon sx={{ marginRight: "5px" }} />
+          {interview.name} ({convertSecondsToHMS(interview.duration)})
         </Box>
       </Box>
     </Grid>
